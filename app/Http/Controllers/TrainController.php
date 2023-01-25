1003f5619e3f5cdbbd44bfc4ef9d2bc213abdcc1
TomasBelfonte <tomas.belfonte@gmail.com>
2023-01-25 10:29:00 +0100

    complete coding

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -8,8 +8,9 @@ use App\Models\Train;
 class TrainController extends Controller
 {
     public function train() {
-        // $trains = Train::where("Orario di Partenza", ("2023 - 01 - 24"))->get();
-        $trains = Train::all();
+        $trains = Train::whereDate("Orario di Partenza", "2023-01-24")->get();
+
+        // $trains = Train::all();
 
         dump($trains);
 
